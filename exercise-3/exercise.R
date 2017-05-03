6dd6ce1f51877431e952d32b4b391ea329d2ed15
Kinsley <kins.tran@comcast.net>
2017-05-03 16:04:20 -0700

    Whoops

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,30 +1,31 @@
 # Install Plotly 
-
+library(plotly)
 
 # Use the plot_ly function to test it out and see that it is working
 # create a variable and pass in the plot_ly function that takes in 
 # the data midwest, sets the x to ~percollege, the color to ~state 
 # and the type to "box" 
-
+plot_ly(data = midwest, x = ~percollege, color = ~state, type = "box")
 
 # Make two vectors of numbers between 1 and 10, with each vector having 10 numbers in it
-
+numbers1 <- c(1:10)
+numbers2 <- c(1:10)
 
 # create a data frame of the two vectors 
-
+numbers.data <- data.frame(numbers1, numbers2)
 
 # now create a basic scatterplot using the data frame you just created, passing it into 
 # a variable
-
+scatter.plot <- plot_ly(data = numbers.data, x = ~numbers1, y = ~numbers2, type = "scatter")
 
 # Call your variable and enjoy your awesome plot! 
-
+scatter.plot
 
 # Now create a new variable styled.scatterplot. Using the same data and scatterplot
 # as from earlier, but you are going to change the markers.  Make it so the 
 # markers are size 10, of a color of your choice.  Finally change the boarder,
 # of the markers to a differnet color with a widthe of 2
-
+styled.scatterplot <- 
 
 # Call the style.scatterplot variable and see what has changed. Finally add
 # add a pipe operator and use the layout function to add a title and set the
